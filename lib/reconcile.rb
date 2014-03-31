a65a0fb0a773bc47caf89169318012d71b1ce5a6
Jason Stirnaman <stirnamanj@gmail.com>
2014-03-30 21:54:45 -0500

    Changed get to get_response.

---
+++ lib/reconcile.rb
@@ -0,0 +1,39 @@
+# Reconciliation Service for ORCID Public Search API
+require 'uri'
+require 'net/http'
+require 'nokogiri'
+require 'jbuilder'
+
+module OrcidReconciliation
+
+def orcid_base
+  orcid_base = 'http://pub.sandbox.orcid.org/v1.1/search/orcid-bio'
+end
+
+def service_metadata
+service_metadata = { "name" => "Simple ORCID Reconciliation", "identifierSpace" => "profile.id", "schemaSpace" => "profile.id", "defaultTypes" => { "id" => "/profile/id", "name" => "Identifier" } }
+end
+
+def get
+  search
+end
+
+def to_json
+d = get.body
+doc = Nokogiri::XML(d)
+doc.css("orcid-search-result orcid-profile")
+end
+
+def search
+  uri = URI(orcid_base)
+  Net::HTTP.get_response(uri)
+end
+
+def to_jsonp
+end
+
+end
+
+class OrcidId
+  include OrcidReconciliation
+end
